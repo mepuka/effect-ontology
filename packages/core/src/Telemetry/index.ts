4524fc3f4b6ca884e92e57587e6f345845931552
mepuka <kokokessy@gmail.com>
2025-11-22 03:21:15 -0800

    feat(telemetry): add Telemetry module exports

---
+++ packages/core/src/Telemetry/index.ts
@@ -0,0 +1,13 @@
+/**
+ * Telemetry Module
+ *
+ * OpenTelemetry integration for LLM observability.
+ *
+ * @module Telemetry
+ * @since 1.0.0
+ */
+
+export * from "./CostCalculator.js"
+export * from "./LlmAttributes.js"
+export * from "./TracingContext.js"
+export * from "./Tracing.js"
