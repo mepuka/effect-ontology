01ab51cef67c4b6758108d410b54ebf722917f3e
mepuka <kokokessy@gmail.com>
2025-12-21 11:33:17 -0800

    conductor(checkpoint): Refactor EntityResolutionService to Effect.Service

--- packages/@core-v2/src/Service/EntityResolution.ts
+++ packages/@core-v2/src/Service/EntityResolution.ts
@@ -15,49 +15,34 @@ import { buildEntityResolutionGraph } from "../Workflow/EntityResolutionGraph.js
 import { EmbeddingService, EmbeddingServiceDefault } from "./Embedding.js"
 
 /**
- * EntityResolutionService interface
- *
- * @since 2.0.0
- * @category Service
- */
-export interface EntityResolutionServiceMethods {
-  readonly resolve: (
-    graphs: ReadonlyArray<KnowledgeGraph>,
-    config: EntityResolutionConfig
-  ) => Effect.Effect<EntityResolutionGraph, never, EmbeddingService>
-}
-
-/**
  * EntityResolutionService - Entity resolution operations
  *
  * @since 2.0.0
  * @category Services
  */
-export class EntityResolutionService extends Context.Tag("@core-v2/EntityResolutionService")<
-  EntityResolutionService,
-  EntityResolutionServiceMethods
->() {
-  static readonly Default: Layer.Layer<EntityResolutionService, never, EmbeddingService> = Layer.effect(
-    EntityResolutionService,
-    Effect.gen(function*() {
-      const _embedding = yield* EmbeddingService
-      return {
-        resolve: (graphs: ReadonlyArray<KnowledgeGraph>, config: EntityResolutionConfig) =>
-          Effect.gen(function*() {
-            // Merge all graphs
-            const mergedEntities = graphs.flatMap((g) => g.entities)
-            const mergedRelations = graphs.flatMap((g) => g.relations)
+const makeEntityResolutionService = Effect.gen(function*() {
+  const _embedding = yield* EmbeddingService
+  return {
+    resolve: (graphs: ReadonlyArray<KnowledgeGraph>, config: EntityResolutionConfig) =>
+      Effect.gen(function*() {
+        // Merge all graphs
+        const mergedEntities = graphs.flatMap((g) => g.entities)
+        const mergedRelations = graphs.flatMap((g) => g.relations)
 
-            const mergedGraph = new KnowledgeGraph({
-              entities: mergedEntities,
-              relations: mergedRelations
-            })
+        const mergedGraph = new KnowledgeGraph({
+          entities: mergedEntities,
+          relations: mergedRelations
+        })
 
-            return yield* buildEntityResolutionGraph(mergedGraph, config)
-          })
-      }
-    })
-  )
+        return yield* buildEntityResolutionGraph(mergedGraph, config)
+      })
+  }
+})
 
-  static readonly Live = EntityResolutionService.Default.pipe(Layer.provide(EmbeddingServiceDefault))
+export class EntityResolutionService extends Effect.Service<EntityResolutionService>()("@core-v2/EntityResolutionService", {
+  effect: makeEntityResolutionService,
+  dependencies: [EmbeddingServiceDefault],
+  accessors: true
+}) {
+  static readonly Live = EntityResolutionService.Default
 }
